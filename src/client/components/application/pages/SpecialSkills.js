1cee50f9d50af8b9f3cacdda459de99aad1de7f9
abdal90 <30297851+abdal90@users.noreply.github.com>
2018-10-20 01:47:30 -0700

    can add new certificate re-upload

--- src/client/components/application/pages/SpecialSkills.js
+++ src/client/components/application/pages/SpecialSkills.js
@@ -1,145 +1,164 @@
 import React, { Component, Fragment } from 'react';
 import { inject, observer } from 'mobx-react';
 import { withRouter } from 'react-router';
-import {
-  withStyles,
-  Grid,
-  Typography,
-  TextField
-} from '@material-ui/core';
+import { Button, Typography, Collapse, Grid, TextField, withStyles } from '@material-ui/core';
+import { Add } from '@material-ui/icons';
 import PropTypes from 'prop-types';
 import RootState from '../../../store/RootState';
-import Button from '@material-ui/core/Button/Button';
+import ValidatedTextField from './components/ValidatedTextField';
+import SpecialSkillsTable from './components/SpecialSkillsTable';
 
-
-const styles = () => ({
-  typography: {
-    display: 'flex',
-    justifyContent: 'flex-start',
-  },
-  formControlLabel: {
-    display: 'flex',
-    justifyContent: 'flex-start',
-  },
-  formControl: {
-    minWidth: 120,
-  },
+// Component Styles
+const styles = theme => ({
+  padding: {
+    padding: theme.spacing.unit * 2
+  }
 });
+
 
 @inject('store')
 @observer
 class SpecialSkills extends Component {
-
   state = {
-
-    skills: "",
-
-    certificates: [{ name: "" }, { issued: "" }, { expire: "" }],
-
-    // field should not show unless clicked
-    showField: false
+    adding: false
   };
 
-  toggleField = (event) => {
-    const doesShow = this.state.showField;
-    this.setState({showField: !doesShow})
+  /**
+   * Open the form
+   */
+  openForm = () => {
+    this.setState({ adding: true });
+  };
+
+  /**
+   * Close and clear the form
+   */
+  closeForm = () => {
+    this.setState({ adding: false });
+    const { store } = this.props;
+    store.application.specialSkills.clear();
   };
 
   render() {
-    const { classes } = this.props;
+    const { adding } = this.state;
+    const { store } = this.props;
+    const {
+      certificate,
+      name: name,
+      issuedDate: issuedDate,
+      expirationDate: expirationDate,
+      description,
+      save,
+    } = store.application.specialSkills;
 
     return (
       <Fragment>
-        <Grid container spacing={24}>
-          <Grid item xs={12}>
-            <Typography variant="subheading" gutterBottom>
-              Special Skills:
-            </Typography>
+        <Grid item xs={12}>
+          <Typography variant="subheading">
+            Skills:
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+           <TextField
+            state={description}
+            multiline
+            rowsMax="4"
+            margin="normal"
+            helperText="Description"
+            id="description"
+            name="description"
+            label="Description of skills"
+          />
+
+         {/* <ValidatedTextField
+            state={description}
+            multiline
+            rowsMax="4"
+            margin="normal"
+            helperText="Description"
+            id="description"
+            name="description"
+            label="Description of skills"
+          />*/}
+        </Grid>
+
+        <Grid item xs={12}>
+          <Typography variant="subheading">
+            Certifications
+          </Typography>
+        </Grid>
+
+        <Collapse in={!adding}>
+          <Grid container spacing={24}>
+            {certificate.length > 0 && (
+              <Grid item xs={12}>
+                <SpecialSkillsTable />
+              </Grid>
+            )}
+            <Grid item xs={12}>
+              <Button
+                variant="raised"
+                color="secondary"
+                onClick={this.openForm}
+              >
+                <Add /> Add Certificate
+              </Button>
+            </Grid>
           </Grid>
 
-          {/*skills entered here*/}
-            <Grid item xs={6}>
-              <Typography variant="headline" gutterBottom>
-                Skills:
-              </Typography>
-            </Grid>
-
-            <Grid item xs={6}>
-              <TextField
-                id="outlined-adornment-name"
-                //className={classNames(classes.margin, classes.textField)}
-                variant="outlined"
-                label="Enter skills"
-                //value={this.state.skills}
-               // onChange={this.handleChange("skills")}
-                helperText="Skills: "
+        </Collapse>
+        <Collapse in={adding}>
+          <Grid container spacing={24}>
+            <Grid item xs={12} sm={12}>
+              <ValidatedTextField
+                state={name}
+                id="name"
+                name="name"
+                label="name of certificate"
               />
             </Grid>
 
-
-          <Grid item xs={9}>
-            <Grid
-              container
-              direction="column"
-              justify="center"
-              alignItems="center"
-            >
-              <Typography variant="headline" gutterBottom>
-                Certificate:
-              </Typography>
-
-              <Grid item xs={6}>
-                <TextField
-                  id="outlined-adornment-name"
-                  //className={classNames(classes.margin, classes.textField)}
-                  variant="outlined"
-                  label="Name"
-                  //value={this.state.weight}
-                  //onChange={this.handleChange("name")}
-                  helperText="Name: "
-                />
-
-                <TextField
-                  id="outlined-adornment-issued"
-                  //className={classNames(classes.margin, classes.textField)}
-                  variant="outlined"
-                  label="Date Issued"
-                  //value={this.state.certificates.issued}
-                  //onChange={this.handleChange("issued")}
-                  helperText="Date Issued: "
-                />
-
-                <TextField
-                  id="outlined-adornment-expire"
-                  //className={classNames(classes.margin, classes.textField)}
-                  variant="outlined"
-                  label="Expiration Date"
-                  //value={this.state.certificates.expire}
-                  //onChange={this.handleChange("expire")}
-                  helperText="Expiration Date:"
-                />
-              </Grid>
+            <Grid item xs={12} sm={12}>
+              <ValidatedTextField
+                state={issuedDate}
+                id="issuedDate"
+                name="issuedDate"
+                label="issued Date"
+                type="date"
+                InputLabelProps={{ shrink: true }}
+              />
             </Grid>
+
+            <Grid item xs={12} sm={12}>
+              <ValidatedTextField
+                state={expirationDate}
+                id="expirationDate"
+                name="expirationDate"
+                label="expiration Date"
+                type="date"
+                InputLabelProps={{ shrink: true }}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  if (save()) {
+                    this.closeForm();
+                  }
+                }}
+              >Save
+              </Button>
+              <Button
+                onClick={this.closeForm}
+              >Cancel
+              </Button>
+            </Grid>
+
           </Grid>
-
-
-            <Grid item xs={3}>
-              <div>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  //className={classes.button}
-                  //onClick={this.toggleField()}
-                >
-                  Add more
-                </Button>
-              </div>
-            </Grid>
-
-
-
-
-        </Grid>
+        </Collapse>
       </Fragment>
     );
   }
